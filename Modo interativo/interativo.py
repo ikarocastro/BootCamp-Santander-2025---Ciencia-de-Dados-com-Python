43838d3d442ec5ddfb0d391a01e27babcfe3063b
Your Name <nunesikaro@gmail.com>
2025-11-21 20:41:33 -0300

    adiocinei modo interativo em python

---
+++ Modo interativo/interativo.py
@@ -0,0 +1,8 @@
+#temos duas formas de entrar no modo  interativo do Python
+#1 - Usando o terminal do sistema operacional
+#2 - Usando uma flag do interpretador Python
+#No terminal do sistema operacional, basta digitar "python" ou "python3", dependendo da sua instalação
+#No segundo caso, podemos usar a flag "-i" ao executar um script Python
+#Exemplo: python -i meu_script.py
+#Isso fará com que o interpretador entre no modo interativo após a execução do script
+
