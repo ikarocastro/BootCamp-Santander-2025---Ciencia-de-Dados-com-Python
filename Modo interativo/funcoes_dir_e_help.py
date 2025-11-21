2d8936615980f17264671fb4f09323f228620bc5
Your Name <nunesikaro@gmail.com>
2025-11-21 20:43:44 -0300

    Add Modo interativo/funcoes_dir_e_help.py (dir and help examples)

---
+++ Modo interativo/funcoes_dir_e_help.py
@@ -0,0 +1,14 @@
+#dir sem argummentos, retorna a lista de nomes no escopo local atual, com um argummento, retorna uma lista de atributos válidos para o objeto 
+#Exemplo:
+
+dir()
+dir(100)
+
+#help - invoca o sistema de ajuda integrado do Python
+#é possivel fazer buscas em modo interativo ou informar por parâmetro o nome de um módulo, função, classe, método ou . variável ou palavra-chave para obter ajuda específica
+#Exemplo:
+
+help()
+help(100) #no terminal, irá abrir a documentação do objeto 100
+
+# A letra Q (de quit) pode ser usada para sair do sistema de ajuda
