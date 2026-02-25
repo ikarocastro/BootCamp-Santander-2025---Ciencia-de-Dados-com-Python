911cb33a32d78c5bdae9871e0f624727b1df5568
Ikaro Castro <nunesikaro@gmail.com>
2026-02-25 12:53:26 -0300

    Usei git clone

---
+++ Introdução_a_Ciencias_de_Dados/operadores_aritimeticos/OA.py
@@ -0,0 +1,15 @@
+produto_1 = 20;
+produto_2 = 10;
+
+print("Soma: ", produto_1 + produto_2);
+print("Subtração: ", produto_1 - produto_2);
+print("Multiplicação: ", produto_1 * produto_2);
+print("Divisão: ", produto_1 // produto_2);
+print("Resto da divisão: ", produto_1 % produto_2);
+print("Exponenciação: ", produto_1 ** produto_2);
+
+x = (10 + 5) * 4;
+y = (10 / 2) + (25 * 2) - (2 ** 2);
+
+print("Valor de x: ", x);
+print("Valor de y: ", y);
